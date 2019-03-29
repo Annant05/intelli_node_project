c02fb2f3339ed00ea11967a769110ed64a1224cc
Annant05 <annantguptaneema@gmail.com>
2019-03-29 14:21:38 +0530

    added pages routes in app.js

--- routes/index_router.js
+++ routes/index_router.js
@@ -21,4 +21,59 @@ router.get('/index', function (req, res) {
     res.render('index') //, {TITLE: TITLE});
 });
 
+
+// for more pages in the dashboard
+
+router.get('/alerts', function (req, res) {
+    console.log("GET: '/alerts' = Render Web-Page");
+    // res.cookie('username', "Annant Gupta",
+    //     {
+    //         maxAge: 900000
+    //     });
+    res.render('alerts') //, {TITLE: TITLE});
+});
+
+
+router.get('/past_records', function (req, res) {
+    console.log("GET: '/past_records' = Render Web-Page");
+    // res.cookie('username', "Annant Gupta",
+    //     {
+    //         maxAge: 900000
+    //     });
+    res.render('past_records') //, {TITLE: TITLE});
+});
+
+
+router.get('/billing', function (req, res) {
+    console.log("GET: '/billing' = Render Web-Page");
+    // res.cookie('username', "Annant Gupta",
+    //     {
+    //         maxAge: 900000
+    //     });
+    res.render('billing') //, {TITLE: TITLE});
+});
+
+
+router.get('/monitoring', function (req, res) {
+    console.log("GET: '/monitoring' = Render Web-Page");
+    // res.cookie('username', "Annant Gupta",
+    //     {
+    //         maxAge: 900000
+    //     });
+    res.render('monitoring') //, {TITLE: TITLE});
+});
+
+
+router.get('/support', function (req, res) {
+    console.log("GET: '/support' = Render Web-Page");
+    // res.cookie('username', "Annant Gupta",
+    //     {
+    //         maxAge: 900000
+    //     });
+    res.render('support') //, {TITLE: TITLE});
+});
+
+
+
+
 module.exports = router;
