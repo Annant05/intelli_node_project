e1e0433fc5075d8190dc903f7bc70c60a04e0268
Annant05 <annantguptaneema@gmail.com>
2019-04-19 00:05:15 +0530

    optimized algo to communicate between esp and arduino. bug fix needed

--- dynamo_mqtt/dynamo_mqtt.ino
+++ dynamo_mqtt/dynamo_mqtt.ino
@@ -5,13 +5,15 @@
 #else
 #error Platform not supported
 #endif
+
 #include <WiFiClientSecure.h>
 #include <MQTT.h>
 #include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson (use v6.xx)
 #include <time.h>
 
 #include <SoftwareSerial.h>
-SoftwareSerial NodeMCU(D7,D8);
+
+SoftwareSerial NodeMCU(D7, D8);
 
 #define emptyString String()
 
@@ -24,12 +26,10 @@ SoftwareSerial NodeMCU(D7,D8);
 #endif
 
 const int MQTT_PORT = 8883;
-// const char MQTT_SUB_TOPIC[] = "$aws/things/" THINGNAME "/shadow/update";
-const char MQTT_PUB_TOPIC[] = "$aws/rules/dynamotopic/my/topic" ; //THINGNAME "/shadow/update";
-//const char MQTT_PUB_TOPIC[] = "$aws/things/" THINGNAME "/shadow/update";
 
+const char MQTT_PUB_INTELLI_DATA[] = "$aws/rules/intelli_datadb/fivesec";
+const char MQTT_PUB_INTELLI_DEVICES[] = "$aws/rules/intelli_devicesdb/onesec";
 
-char reading[5];
 
 #ifdef USE_SUMMER_TIME_DST
 uint8_t DST = 1;
@@ -51,184 +51,192 @@ unsigned long lastMillis = 0;
 time_t now;
 time_t nowish = 1510592825;
 
-float reading5Sec;
+//float reading5Sec;
+//float MAIN_READING;
 
-void NTPConnect(void)
-{
-  Serial.print("Setting netime using SNTP");
-  configTime(TIME_ZONE * 3600, DST * 3600, "pool.ntp.org", "time.nist.gov");
-  now = time(nullptr);
-  while (now < nowish)
-  {
-    delay(500);
-    Serial.print(".");
+void NTPConnect(void) {
+    Serial.print("Setting netime using SNTP");
+    configTime(TIME_ZONE * 3600, DST * 3600, "pool.ntp.org", "time.nist.gov");
     now = time(nullptr);
-  }
-  Serial.println("done!");
-  struct tm timeinfo;
-  gmtime_r(&now, &timeinfo);
-  Serial.print("Current time: ");
-  Serial.print(asctime(&timeinfo));
-}
-
-void messageReceived(String &topic, String &payload)
-{
-  Serial.println("Recieved [" + topic + "]: " + payload);
-}
-
-void lwMQTTErr(lwmqtt_err_t reason)
-{
-  if (reason == lwmqtt_err_t::LWMQTT_SUCCESS)
-    Serial.print("Success");
-  else if (reason == lwmqtt_err_t::LWMQTT_BUFFER_TOO_SHORT)
-    Serial.print("Buffer too short");
-  else if (reason == lwmqtt_err_t::LWMQTT_VARNUM_OVERFLOW)
-    Serial.print("Varnum overflow");
-  else if (reason == lwmqtt_err_t::LWMQTT_NETWORK_FAILED_CONNECT)
-    Serial.print("Network failed connect");
-  else if (reason == lwmqtt_err_t::LWMQTT_NETWORK_TIMEOUT)
-    Serial.print("Network timeout");
-  else if (reason == lwmqtt_err_t::LWMQTT_NETWORK_FAILED_READ)
-    Serial.print("Network failed read");
-  else if (reason == lwmqtt_err_t::LWMQTT_NETWORK_FAILED_WRITE)
-    Serial.print("Network failed write");
-  else if (reason == lwmqtt_err_t::LWMQTT_REMAINING_LENGTH_OVERFLOW)
-    Serial.print("Remaining length overflow");
-  else if (reason == lwmqtt_err_t::LWMQTT_REMAINING_LENGTH_MISMATCH)
-    Serial.print("Remaining length mismatch");
-  else if (reason == lwmqtt_err_t::LWMQTT_MISSING_OR_WRONG_PACKET)
-    Serial.print("Missing or wrong packet");
-  else if (reason == lwmqtt_err_t::LWMQTT_CONNECTION_DENIED)
-    Serial.print("Connection denied");
-  else if (reason == lwmqtt_err_t::LWMQTT_FAILED_SUBSCRIPTION)
-    Serial.print("Failed subscription");
-  else if (reason == lwmqtt_err_t::LWMQTT_SUBACK_ARRAY_OVERFLOW)
-    Serial.print("Suback array overflow");
-  else if (reason == lwmqtt_err_t::LWMQTT_PONG_TIMEOUT)
-    Serial.print("Pong timeout");
-}
-
-void lwMQTTErrConnection(lwmqtt_return_code_t reason)
-{
-  if (reason == lwmqtt_return_code_t::LWMQTT_CONNECTION_ACCEPTED)
-    Serial.print("Connection Accepted");
-  else if (reason == lwmqtt_return_code_t::LWMQTT_UNACCEPTABLE_PROTOCOL)
-    Serial.print("Unacceptable Protocol");
-  else if (reason == lwmqtt_return_code_t::LWMQTT_IDENTIFIER_REJECTED)
-    Serial.print("Identifier Rejected");
-  else if (reason == lwmqtt_return_code_t::LWMQTT_SERVER_UNAVAILABLE)
-    Serial.print("Server Unavailable");
-  else if (reason == lwmqtt_return_code_t::LWMQTT_BAD_USERNAME_OR_PASSWORD)
-    Serial.print("Bad UserName/Password");
-  else if (reason == lwmqtt_return_code_t::LWMQTT_NOT_AUTHORIZED)
-    Serial.print("Not Authorized");
-  else if (reason == lwmqtt_return_code_t::LWMQTT_UNKNOWN_RETURN_CODE)
-    Serial.print("Unknown Return Code");
-}
-
-void connectToMqtt(bool nonBlocking = false)
-{
-  Serial.print("MQTT connecting ");
-  while (!client.connected())
-  {
-    if (client.connect(THINGNAME))
-    {
-      Serial.println("connected!");
-      // if (!client.subscribe(MQTT_SUB_TOPIC))
-        // lwMQTTErr(client.lastError());
+    while (now < nowish) {
+        delay(500);
+        Serial.print(".");
+        now = time(nullptr);
     }
-    else
-    {
-      Serial.print("SSL Error Code: ");
-      Serial.println(net.getLastSSLError());
-      Serial.print("failed, reason -> ");
-      lwMQTTErrConnection(client.returnCode());
-      if (!nonBlocking)
-      {
-        Serial.println(" < try again in 5 seconds");
-        delay(5000);
-      }
-      else
-      {
-        Serial.println(" <");
-      }
+    Serial.println("done!");
+    struct tm timeinfo;
+    gmtime_r(&now, &timeinfo);
+    Serial.print("Current time: ");
+    Serial.print(asctime(&timeinfo));
+}
+
+void messageReceived(String &topic, String &payload) {
+    Serial.println("Recieved [" + topic + "]: " + payload);
+}
+
+void lwMQTTErr(lwmqtt_err_t reason) {
+    if (reason == lwmqtt_err_t::LWMQTT_SUCCESS)
+        Serial.print("Success");
+    else if (reason == lwmqtt_err_t::LWMQTT_BUFFER_TOO_SHORT)
+        Serial.print("Buffer too short");
+    else if (reason == lwmqtt_err_t::LWMQTT_VARNUM_OVERFLOW)
+        Serial.print("Varnum overflow");
+    else if (reason == lwmqtt_err_t::LWMQTT_NETWORK_FAILED_CONNECT)
+        Serial.print("Network failed connect");
+    else if (reason == lwmqtt_err_t::LWMQTT_NETWORK_TIMEOUT)
+        Serial.print("Network timeout");
+    else if (reason == lwmqtt_err_t::LWMQTT_NETWORK_FAILED_READ)
+        Serial.print("Network failed read");
+    else if (reason == lwmqtt_err_t::LWMQTT_NETWORK_FAILED_WRITE)
+        Serial.print("Network failed write");
+    else if (reason == lwmqtt_err_t::LWMQTT_REMAINING_LENGTH_OVERFLOW)
+        Serial.print("Remaining length overflow");
+    else if (reason == lwmqtt_err_t::LWMQTT_REMAINING_LENGTH_MISMATCH)
+        Serial.print("Remaining length mismatch");
+    else if (reason == lwmqtt_err_t::LWMQTT_MISSING_OR_WRONG_PACKET)
+        Serial.print("Missing or wrong packet");
+    else if (reason == lwmqtt_err_t::LWMQTT_CONNECTION_DENIED)
+        Serial.print("Connection denied");
+    else if (reason == lwmqtt_err_t::LWMQTT_FAILED_SUBSCRIPTION)
+        Serial.print("Failed subscription");
+    else if (reason == lwmqtt_err_t::LWMQTT_SUBACK_ARRAY_OVERFLOW)
+        Serial.print("Suback array overflow");
+    else if (reason == lwmqtt_err_t::LWMQTT_PONG_TIMEOUT)
+        Serial.print("Pong timeout");
+}
+
+void lwMQTTErrConnection(lwmqtt_return_code_t reason) {
+    if (reason == lwmqtt_return_code_t::LWMQTT_CONNECTION_ACCEPTED)
+        Serial.print("Connection Accepted");
+    else if (reason == lwmqtt_return_code_t::LWMQTT_UNACCEPTABLE_PROTOCOL)
+        Serial.print("Unacceptable Protocol");
+    else if (reason == lwmqtt_return_code_t::LWMQTT_IDENTIFIER_REJECTED)
+        Serial.print("Identifier Rejected");
+    else if (reason == lwmqtt_return_code_t::LWMQTT_SERVER_UNAVAILABLE)
+        Serial.print("Server Unavailable");
+    else if (reason == lwmqtt_return_code_t::LWMQTT_BAD_USERNAME_OR_PASSWORD)
+        Serial.print("Bad UserName/Password");
+    else if (reason == lwmqtt_return_code_t::LWMQTT_NOT_AUTHORIZED)
+        Serial.print("Not Authorized");
+    else if (reason == lwmqtt_return_code_t::LWMQTT_UNKNOWN_RETURN_CODE)
+        Serial.print("Unknown Return Code");
+}
+
+void connectToMqtt(bool nonBlocking = false) {
+    Serial.print("MQTT connecting ");
+    while (!client.connected()) {
+        if (client.connect(THINGNAME)) {
+            Serial.println("connected!");
+            // if (!client.subscribe(MQTT_SUB_TOPIC))
+            // lwMQTTErr(client.lastError());
+        } else {
+            Serial.print("SSL Error Code: ");
+            Serial.println(net.getLastSSLError());
+            Serial.print("failed, reason -> ");
+            lwMQTTErrConnection(client.returnCode());
+            if (!nonBlocking) {
+                Serial.println(" < try again in 5 seconds");
+                delay(5000);
+            } else {
+                Serial.println(" <");
+            }
+        }
+        if (nonBlocking)
+            break;
     }
-    if (nonBlocking)
-      break;
-  }
 }
 
-void connectToWiFi(String init_str)
-{
-  if (init_str != emptyString)
-    Serial.print(init_str);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(1000);
-  }
-  if (init_str != emptyString)
-    Serial.println("ok!");
+void connectToWiFi(String init_str) {
+    if (init_str != emptyString)
+        Serial.print(init_str);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(1000);
+    }
+    if (init_str != emptyString)
+        Serial.println("ok!");
 }
 
-void checkWiFiThenMQTT(void)
-{
-  connectToWiFi("Checking WiFi");
-  connectToMqtt();
+void checkWiFiThenMQTT(void) {
+    connectToWiFi("Checking WiFi");
+    connectToMqtt();
 }
 
 unsigned long previousMillis = 0;
 const long interval = 5000;
 
 
-void sendData(void)
-{
-  DynamicJsonDocument jsonBuffer(JSON_OBJECT_SIZE(3) + 100);
-  JsonObject root = jsonBuffer.to<JsonObject>();
-//  JsonObject state = root.createNestedObject("state");
-//  JsonObject state_reported = state.createNestedObject("reported");
-//  state_reported["value"] = random(100);
+void sendData(void) {
 
-//if(Serial.available() > 0 ){
-//   delay(100); //allows all serial sent to be received together
-//    while(Serial.available() && i<5) {
-//     watt[i++] = Serial.read();
-//    }
-//    root["watt"] = watt[i++];
-//    watt[i++]='\0';
+    DynamicJsonDocument jsonBuffer(JSON_OBJECT_SIZE(3) + 100);
+    DynamicJsonDocument jsonBuffer2(JSON_OBJECT_SIZE(3) + 100);
 
-     int i=0;
- float valread;
-
-if(NodeMCU.available()  > 0){
- while(NodeMCU.available() && i<5) {
-       reading[i++] = NodeMCU.read();
-  }
-  reading[i++]='\0';
-}
-      valread = atof(reading);
- Serial.println(String("WATT :  ") + valread);
- delay(5000);
-
-     root["ivrs"] = "9806247089" ;
-     root["reading"] = valread;
-//  }
-//   root["temp"] = 100;
-//   root["timestamp"] = random(100);
+    JsonObject sec5data = jsonBuffer.to<JsonObject>();
+    JsonObject oneunit = jsonBuffer2.to<JsonObject>();
 
 
-  Serial.printf("Sending  [%s]: ", MQTT_PUB_TOPIC);
+    char MAIN_DATA[11];
+    char strReading5sec[5], strMAIN_READING[5];
+    float read5sec = 0.00, main_reading = 0.00;
+
+
+    int i=0;
+    if (NodeMCU.available() > 0) {
+        while (NodeMCU.available() && i < 11) {
+            MAIN_DATA[i++] = NodeMCU.read();
+        }
+        MAIN_DATA[i++] = '\0';
+    }
+
+    //    for (int k = 0; k <= 11; k++)Serial.print(MAIN_DATA[i]);
+
+    // get first 5 second reading.
+    for (int i = 1; i != 5; i++)strReading5sec[i - 1] = MAIN_DATA[i];
+    strReading5sec[4] = '\0';
+    read5sec = atof(strReading5sec);
+
+    // if the first char is Y then get this main reading
+    if (MAIN_DATA[0] == 'Y') {
+        for (int i = 1; i != 5; i++)strMAIN_READING[i - 1] = MAIN_DATA[i + 5];
+        strMAIN_READING[4] = '\0';
+//        cout << strMAIN_READING << endl;
+        main_reading = atof(strMAIN_READING);
+        Serial.println(String("MAIN_READING :  ") + main_reading);
+        Serial.printf("Sending  [%s]: ", MQTT_PUB_INTELLI_DATA);
+
+        char shadow[measureJson(oneunit) + 1];
+        serializeJson(oneunit, shadow, sizeof(shadow));
+
+        if (!client.publish(MQTT_PUB_INTELLI_DEVICES, shadow, false, 0))
+            lwMQTTErr(client.lastError());
+
+    }
+
+
+    Serial.println(String("Reading 5 second :  ") + read5sec);
+
+    sec5data["ivrs"] = "7415893478";
+
+
+    if( read5sec != 0.00){
+        sec5data["reading"] = read5sec;
+        Serial.printf("Sending  [%s]: ", MQTT_PUB_INTELLI_DATA);
 //  Serial.printf("\nSending to shadow [%s]: ", MQTT_PUB_SHADOW_TOPIC);
-  Serial.println();
-  char shadow[measureJson(root) + 1];
-  serializeJson(root, shadow, sizeof(shadow));
-  if (!client.publish(MQTT_PUB_TOPIC, shadow, false, 0))
-    lwMQTTErr(client.lastError());
-     digitalWrite(LED_BUILTIN, LOW);
-     delay(100);// turn the LED on (HIGH is the voltage level)
+        Serial.println();
+        char shadow[measureJson(sec5data) + 1];
+        serializeJson(sec5data, shadow, sizeof(shadow));
+
+        if (!client.publish(MQTT_PUB_INTELLI_DATA, shadow, false, 0))
+            lwMQTTErr(client.lastError());
+    }else
+        Serial.println("Data not sent no server cause no data in reading.\n");
+
+    delay(4900);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);// turn the LED on (HIGH is the voltage level)
 //  if (!client.publish(MQTT_PUB_SHADOW_TOPIC, shadow, false, 0))
 //    lwMQTTErr(client.lastError());
-     digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
 }
 
 //void printdatafromserial(){
@@ -251,58 +259,52 @@ if(NodeMCU.available()  > 0){
 
 
 
-void setup()
-{
-  Serial.begin(115200);
+void setup() {
+    Serial.begin(115200);
 
-  NodeMCU.begin(57600);
-  pinMode(D7,INPUT);
-  pinMode(D8,OUTPUT);
+    NodeMCU.begin(115200);
+    pinMode(D7, INPUT);
+    pinMode(D8, OUTPUT);
 
-  delay(5000);
-  Serial.println();
-  Serial.println();
+    delay(5000);
+    Serial.println();
+    Serial.println();
 #ifdef ESP32
-  WiFi.setHostname(THINGNAME);
+    WiFi.setHostname(THINGNAME);
 #else
-  WiFi.hostname(THINGNAME);
+    WiFi.hostname(THINGNAME);
 #endif
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
-  connectToWiFi(String("Attempting to connect to SSID: ") + String(ssid));
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
+    connectToWiFi(String("Attempting to connect to SSID: ") + String(ssid));
 
-  NTPConnect();
+    NTPConnect();
 
 #ifdef ESP32
-  net.setCACert(cacert);
+    net.setCACert(cacert);
   net.setCertificate(client_cert);
   net.setPrivateKey(privkey);
 #else
-  net.setTrustAnchors(&cert);
-  net.setClientRSACert(&client_crt, &key);
+    net.setTrustAnchors(&cert);
+    net.setClientRSACert(&client_crt, &key);
 #endif
 
-  client.begin(MQTT_HOST, MQTT_PORT, net);
-  client.onMessage(messageReceived);
-  connectToMqtt();
+    client.begin(MQTT_HOST, MQTT_PORT, net);
+    client.onMessage(messageReceived);
+    connectToMqtt();
 }
 
-void loop()
-{
-  now = time(nullptr);
-  if (!client.connected())
-  {
-    checkWiFiThenMQTT();
-    //checkWiFiThenMQTTNonBlocking();
-    //checkWiFiThenReboot();
-  }
-  else
-  {
-    client.loop();
-    if (millis() - lastMillis > 5000)
-    {
-      lastMillis = millis();
-      sendData();
+void loop() {
+    now = time(nullptr);
+    if (!client.connected()) {
+        checkWiFiThenMQTT();
+        //checkWiFiThenMQTTNonBlocking();
+        //checkWiFiThenReboot();
+    } else {
+        client.loop();
+        if (millis() - lastMillis > 5000) {
+            lastMillis = millis();
+            sendData();
+        }
     }
-  }
 }
